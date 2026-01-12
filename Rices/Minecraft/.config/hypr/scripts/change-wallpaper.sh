78e14142f91fa2a2326df3646607b5a206172984
Niko~ <Niko3636@proton.me>
2026-01-12 16:14:30 +1100

    wallpaper picker is being weirdd, not enough brainpower to fix it

--- Rices/Minecraft/.config/hypr/scripts/change-wallpaper.sh
+++ Rices/Minecraft/.config/hypr/scripts/change-wallpaper.sh
@@ -6,13 +6,13 @@ directory=~/.config/hypr/wallpapers/
 if [ -z "$1" ]; then
     if [ -d "$directory" ]; then
         random_background=$(ls $directory/*.jpg | shuf -n 1)
-        swww img --transition-type grow --transition-pos top-right $random_background
+        swww img --transition-type any $random_background
         rm ~/.config/rofi/.current_wallpaper
         cp $random_background ~/.config/rofi/.current_wallpaper
     fi
 else
     
-    swww img --transition-type grow --transition-pos top-right "$directory/$1"
+    swww img --transition-type any "$directory/$1"
     rm ~/.config/rofi/.current_wallpaper
     cp "$directory/$1" ~/.config/rofi/.current_wallpaper
 fi
--- Rices/Minecraft/.config/rofi/wallpaper_picker.sh
+++ Rices/Minecraft/.config/rofi/wallpaper_picker.sh
@@ -21,7 +21,7 @@
 
 # Set some variables
 wall_dir="${HOME}/.config/hypr/wallpapers/"
-cacheDir="${HOME}/.cache/wallpicker/$(cat ~/.current_theme)"
+cacheDir="${HOME}/.cache/wallpicker/$(cat ~/.current_rice_theme)"
 rofi_command="rofi -x11 -dmenu -theme ${HOME}/.config/rofi/wall-select.rasi -theme-str ${rofi_override}"
 
 # Create cache dir if not exists
@@ -40,15 +40,15 @@ rofi_override="element-icon{size:${monitor_res}px;border-radius:0px;}"
 # Convert images in directory and save to cache dir
 for imagen in "$wall_dir"/*.{jpg,jpeg,png,webp}; do
 	if [ -f "$imagen" ]; then
-		nombre_archivo=$(basename "$imagen")
-			if [ ! -f "${cacheDir}/${nombre_archivo}" ] ; then
-				convert -strip "$imagen" -thumbnail 400x400^ -gravity center -extent 400x400 "${cacheDir}/${nombre_archivo}"
+		file_name=$(basename "$imagen")
+			if [ ! -f "${cacheDir}/${file_name}" ] ; then
+				convert -strip "$imagen" -thumbnail 400x400^ -gravity center -extent 400x400 "${cacheDir}/${file_name}"
 			fi
     fi
 done
 
 # Select a picture with rofi
-wall_selection=$(find "${wall_dir}"  -maxdepth 1  -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -exec basename {} \; | sort | while read -r A ; do  echo -en "$A\x00icon\x1f""${cacheDir}"/"$A\n" ; done | $rofi_command)
+wall_selection=$(find "${wall_dir}"  -maxdepth 1  -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -exec basename {} \; | sort | while read -r A ; do  echo -en "$A""${cacheDir}"/"$A\n" ; done | $rofi_command)
 
 # Set the wallpaper
 [[ -n "$wall_selection" ]] || exit 1
